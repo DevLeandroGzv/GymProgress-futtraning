d62f88b61a99e2fce38d22ec9dd5a8716e68f54d
LeandroGonzalez <leandrogonzales@wposs.com>
2026-04-24 16:52:37 -0500

    Datos de prueba
    ** se agregan algunos datos de prueba

--- app/src/main/java/com/leandro/gymprogress_futtraing/presentation/viewmodel/GymUiState.kt
+++ app/src/main/java/com/leandro/gymprogress_futtraing/presentation/viewmodel/GymUiState.kt
@@ -2,9 +2,9 @@ package com.leandro.gymprogress_futtraing.presentation.viewmodel
 
 import com.leandro.gymprogress_futtraing.domain.model.Exercise
 
-data class GymUiState(val userName: String = "Tu Nombre",
-                      val userWeight: String = "0.0",
-                      val userHeight: String = "0",
+data class GymUiState(val userName: String = "Leandro Gonzalez",
+                      val userWeight: String = "91",
+                      val userHeight: String = "1.82",
                       val legExercises: List<Exercise> = emptyList(),
                       val torsoExercises: List<Exercise> = emptyList(),
                       val isLoading: Boolean = false)
